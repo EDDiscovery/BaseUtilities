e60f1a510d3d610a0b57198eb265e44cbb76476f
robbyxp1 <robxp@hotmail.com>
2019-10-29 15:06:01 +0000

    Reorganise Code
    Getting too big

--- TestOpenTk/Volumetrics/ShaderTestVolumetric3.cs
+++ TestOpenTk/Volumetrics/ShaderTestVolumetric3.cs
@@ -1,32 +1,25 @@
 ﻿using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
-using OpenTKUtils.GL4;
-using OpenTKUtils.Common;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using OpenTKUtils;
-using System.Reflection;
+using OpenTKUtils.Common;
+using OpenTKUtils.GL4;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 // Demonstrate the volumetric calculations needed to compute a plane facing the user inside a bounding box done inside a geo shader
+// this one add on tex coord calculation and using a single tight quad shows its working
 
 namespace TestOpenTk
 {
-    public partial class ShaderTestVolumetric2 : Form
+    public partial class ShaderTestVolumetric3 : Form
     {
         private Controller3D gl3dcontroller = new Controller3D();
 
         private Timer systemtimer = new Timer();
 
-        public ShaderTestVolumetric2()
+        public ShaderTestVolumetric3()
         {
             InitializeComponent();
 
@@ -62,22 +55,24 @@ void main(void)
 	gl_Position = position;       
     instance = gl_InstanceID;
 }
-";                
-                
+";
+
             string fcode = @"
 #version 450 core
 out vec4 color;
 in vec4 vs_color;
+in vec3 vs_texcoord;
 
 void main(void)
 {
-    color = vs_color;
+    //color = vs_color;
+    color = vec4(vs_texcoord,0.8);
 }
 ";
 
             public ShaderV2()
             {
-                CompileLink(vertex: vcode, frag: fcode, geo: "TestOpenTk.volumetricgeo2.glsl");
+                CompileLink(vertex: vcode, frag: fcode, geo: "TestOpenTk.Volumetrics.volumetricgeo3.glsl");
                 StartAction += (s) => { GLStatics.PointSize(10); };
             }
         }
@@ -114,7 +109,7 @@ void main(void)
 
             gl3dcontroller.MatrixCalc.PerspectiveNearZDistance = 0.1f;
             gl3dcontroller.MatrixCalc.ZoomDistance = 40F;
-            gl3dcontroller.Start(new Vector3(30, 0, 0), new Vector3(90, 0, 0), 1F);
+            gl3dcontroller.Start(new Vector3(0, 0, 0), new Vector3(90, 0, 0), 1F);
 
             gl3dcontroller.TravelSpeed = (ms) =>
             {
@@ -125,9 +120,10 @@ void main(void)
             items.Add("LINEYELLOW", new GLFixedShader(System.Drawing.Color.Yellow, (a) => { GLStatics.LineWidth(1); }));
             items.Add("LINEPURPLE", new GLFixedShader(System.Drawing.Color.Purple, (a) => { GLStatics.LineWidth(1); }));
             items.Add("DOTYELLOW", new GLFixedProjectionShader(System.Drawing.Color.Yellow, (a) => { GLStatics.PointSize(10); }));
-            items.Add("SURFACEBLUE", new GLFixedProjectionShader(System.Drawing.Color.Blue, (a) => {  }));
+            items.Add("SURFACEBLUE", new GLFixedProjectionShader(System.Drawing.Color.Blue, (a) => { }));
 
             items.Add("V2", new ShaderV2());
+            //           items.Add("wooden", new GLTexture2D(Properties.Resources.wooden));
 
             rObjects.Add(items.Shader("COS-1L"), "L1",   // horizontal
                          GLRenderableItem.CreateVector4Color4(items, OpenTK.Graphics.OpenGL4.PrimitiveType.Lines,
@@ -142,6 +138,40 @@ void main(void)
                                                          new Color4[] { Color.Gray })
                                );
 
+            // Number markers using instancing and 2d arrays, each with its own transform
+
+            Bitmap[] numbers = new Bitmap[20];
+            Matrix4[] numberpos = new Matrix4[20];
+
+            Font fnt = new Font("Arial", 44);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int v = -100 + i * 10;
+                numbers[i] = new Bitmap(100, 100);
+                BaseUtils.BitMapHelpers.DrawTextCentreIntoBitmap(ref numbers[i], v.ToString(), fnt, Color.Red, Color.AliceBlue);
+                numberpos[i] = Matrix4.CreateScale(1);
+                numberpos[i] *= Matrix4.CreateRotationX(-80f.Radians());
+                numberpos[i] *= Matrix4.CreateTranslation(new Vector3(20, 0, v));
+            }
+
+            GLTexture2DArray array = new GLTexture2DArray(numbers, ownbitmaps: true);
+            items.Add("Nums", array);
+            items.Add("IC-2", new GLShaderPipeline(new GLVertexShaderTextureMatrixTranslation(), new GLFragmentShaderTexture2DIndexed()));
+            items.Shader("IC-2").StartAction += (s) => { items.Tex("Nums").Bind(1); GL.Disable(EnableCap.CullFace); };
+            items.Shader("IC-2").FinishAction += (s) => { GL.Enable(EnableCap.CullFace); };
+
+            // investigate why its wrapping when we asked for it TexQUAD 1 which should interpolate over surface..
+
+            rObjects.Add(items.Shader("IC-2"), "1-b",
+                                    GLRenderableItem.CreateVector4Vector2Matrix4(items, OpenTK.Graphics.OpenGL4.PrimitiveType.Quads,
+                                            GLShapeObjectFactory.CreateQuad(1.0f), GLShapeObjectFactory.TexQuad, numberpos,
+                                            null, numberpos.Length));
+
+
+
+
+
 
             items.Add("MCUB", new GLMatrixCalcUniformBlock());     // create a matrix uniform block 
 
@@ -149,13 +179,13 @@ void main(void)
 
             Vector4[] points = new Vector4[]
             {
-                new Vector4(20,-5,-10,1),
-                new Vector4(40,+5,+10,1),
+                new Vector4(-40,-20,-40,1),
+                new Vector4(+40,+20,+40,1),
             };
 
-            rObjects.Add(items.Shader("V2"), GLRenderableItem.CreateVector4(items, OpenTK.Graphics.OpenGL4.PrimitiveType.Lines, points,ic:9));
+            rObjects.Add(items.Shader("V2"), GLRenderableItem.CreateVector4(items, OpenTK.Graphics.OpenGL4.PrimitiveType.Lines, points, ic: 1));
 
-            int left = 20, right = 40, bottom = -5, top = +5, front = -10, back = 10;
+            int left = -40, right = 40, bottom = -20, top = +20, front = -40, back = 40;
             Vector4[] lines2 = new Vector4[]
             {
                 new Vector4(left,bottom,front,1),   new Vector4(left,top,front,1),
@@ -178,8 +208,8 @@ void main(void)
             rObjects.Add(items.Shader("LINEYELLOW"),
                         GLRenderableItem.CreateVector4(items, OpenTK.Graphics.OpenGL4.PrimitiveType.Lines, lines2));
 
-            dataoutbuffer = items.NewStorageBlock(5);       
-            dataoutbuffer.Allocate(sizeof(float) * 4 * 32, OpenTK.Graphics.OpenGL4.BufferUsageHint.DynamicRead);
+            dataoutbuffer = items.NewStorageBlock(5);
+            dataoutbuffer.Allocate(sizeof(float) * 4 * 32, OpenTK.Graphics.OpenGL4.BufferUsageHint.DynamicRead);    // 32 vec4 back
 
             atomicbuffer = items.NewAtomicBlock(6);
             atomicbuffer.Allocate(sizeof(float) * 32, OpenTK.Graphics.OpenGL4.BufferUsageHint.DynamicCopy);
@@ -205,22 +235,35 @@ void main(void)
 
             GL.MemoryBarrier(MemoryBarrierFlags.AllBarrierBits);
 
-            Vector4[] databack = dataoutbuffer.ReadVector4(0, 2);
-            for (int i = 0; i < databack.Length; i++)
+            Vector4[] databack = dataoutbuffer.ReadVector4(0, 12);
+
+            System.Diagnostics.Debug.WriteLine("avg {0} txtavg {1}", databack[0].ToStringVec(), databack[1].ToStringVec());
+
+            for (int i = 2; i < databack.Length; i += 2)
             {
-                System.Diagnostics.Debug.WriteLine(i + " = " + databack[i].ToString());
+                System.Diagnostics.Debug.WriteLine("{0} v{1} a{2:0.0} :  {3}", databack[i].X, databack[i].Z, databack[i].Y, databack[i + 1].ToStringVec());
             }
 
             this.Text = "Looking at " + gl3dcontroller.MatrixCalc.TargetPosition + " dir " + gl3dcontroller.Camera.Current + " eye@ " + gl3dcontroller.MatrixCalc.EyePosition + " Dist " + gl3dcontroller.MatrixCalc.EyeDistance;
         }
 
-        private void SystemTick(object sender, EventArgs e )
+        private void SystemTick(object sender, EventArgs e)
         {
-            var cdmt = gl3dcontroller.HandleKeyboard(true);
-            if ( cdmt.AnythingChanged )
+            var cdmt = gl3dcontroller.HandleKeyboard(true, OtherKeys);
+            if (cdmt.AnythingChanged)
                 gl3dcontroller.Redraw();
         }
 
+        private void OtherKeys(BaseUtils.KeyboardState kb)
+        {
+            if (kb.IsPressedRemove(Keys.F1, BaseUtils.KeyboardState.ShiftState.None))
+            {
+                int times = 1000;
+                System.Diagnostics.Debug.WriteLine("Start test");
+                long tickcount = gl3dcontroller.Redraw(times);
+                System.Diagnostics.Debug.WriteLine("Redraw {0} ms per {1}", tickcount, (float)tickcount/(float)times);
+            }
+        }
     }
 }
 
