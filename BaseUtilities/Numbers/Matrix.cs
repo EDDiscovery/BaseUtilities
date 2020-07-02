6e5646075c48a94056f43357a44b82e4f1f0f004
Andrea Spada <spada.andrea@gmail.com>
2020-07-02 19:48:02 -0300

    Add two classes for AstroPlot and other similar tools: Matrix, and HotSpotMap"

---
+++ BaseUtilities/Numbers/Matrix.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace BaseUtils
+{
+    /// <summary>
+    /// http://www.cs.cornell.edu/courses/cs4620/2010fa/lectures/03transforms3d.pdf
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public class Matrix<T>
+    {
+        private readonly int rows;
+        private readonly int columns;
+
+        protected T[,] matrix;
+
+        public Matrix(int n, int m)
+        {
+            matrix = new T[n, m];
+            rows = n;
+            columns = m;
+        }
+
+        public void SetValByIdx(int m, int n, T x)
+        {
+            matrix[n, m] = x;
+        }
+
+        public T GetValByIndex(int n, int m)
+        {
+            return matrix[n, m];
+        }
+
+        public void SetMatrix(T[] arr)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    matrix[r, c] = arr[(r * columns) + c];
+                }
+            }
+        }
+
+        public static Matrix<T> operator |(Matrix<T> m1, Matrix<T> m2)
+        {
+            var m = new Matrix<T>(m1.rows, m1.columns + m2.columns);
+            for (int r = 0; r < m1.rows; r++)
+            {
+                for (int c = 0; c < m1.columns; c++)
+                    m.matrix[r, c] = m1.matrix[r, c];
+                for (int c = 0; c < m2.columns; c++)
+                    m.matrix[r, c + m1.columns] = m2.matrix[r, c];
+            }
+
+            return m;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> m1, Matrix<T> m2)
+        {
+            var m = new Matrix<T>(m1.rows, m2.columns);
+            for (int r = 0; r < m.rows; r++)
+            {
+                for (int c = 0; c < m.columns; c++)
+                {
+                    T tmp = (dynamic)0;
+                    for (int i = 0; i < m2.rows; i++)
+                    {
+                        tmp += (dynamic)m1.matrix[r, i] * (dynamic)m2.matrix[i, c];
+                    }
+
+                    m.matrix[r, c] = tmp;
+                }
+            }
+
+            return m;
+        }
+
+        public static Matrix<T> operator ~(Matrix<T> m)
+        {
+            var tmp = new Matrix<T>(m.columns, m.rows);
+            for (int r = 0; r < m.rows; r++)
+            {
+                for (int c = 0; c < m.columns; c++)
+                {
+                    tmp.matrix[c, r] = m.matrix[r, c];
+                }
+            }
+
+            return tmp;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> m)
+        {
+            var tmp = new Matrix<T>(m.columns, m.rows);
+            for (int r = 0; r < m.rows; r++)
+            {
+                for (int c = 0; c < m.columns; c++)
+                {
+                    tmp.matrix[r, c] = -(dynamic)m.matrix[r, c];
+                }
+            }
+
+            return tmp;
+        }
+    }
+}
+
---
+++ BaseUtilities/WinForms/HotSpotMap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BaseUtils
+{
+    public class HotSpotMap
+    {
+        private Dictionary<string, double[]> _hotSpots = new Dictionary<string, double[]>();
+
+        public void CalculateHotSpotRegions(List<object[]> plotHotSpot, double HotSpotRadius)
+        {
+            _hotSpots.Clear();
+
+            foreach (var hotSpot in plotHotSpot)
+            {
+                _hotSpots.Add(hotSpot[0].ToString(), new double[]
+                {
+                    (double)hotSpot[1] - HotSpotRadius,
+                    (double)hotSpot[1] + HotSpotRadius,
+                    (double)hotSpot[2] - HotSpotRadius,
+                    (double)hotSpot[2] + HotSpotRadius
+                });
+            }
+        }
+
+        public string CheckForMouseInHotSpot(Point mousePosition)
+        {
+            var hotSpotName = "";
+
+            if (_hotSpots != null)
+            {
+                foreach (KeyValuePair<string, double[]> item in _hotSpots)
+                {
+                    if (mousePosition.X > item.Value[0] && mousePosition.X < item.Value[1] &&
+                        mousePosition.Y > item.Value[2] && mousePosition.Y < item.Value[3])
+                    {
+                        hotSpotName = item.Key;
+                        Debug.WriteLine(hotSpotName);
+                    }
+                }
+            }
+
+            //Debug.WriteLine("I'm running!");
+
+            return hotSpotName;
+        }
+    }
+}
